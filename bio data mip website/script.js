e515b8ca3084de15c5e18af5d0a1cf194730b41e
mipterbaru2025-cmd <mipterbaru2025@gmail.com>
2026-02-05 20:47:02 +0700

    Delete bio data mip website/script.js

--- bio data mip website/script.js
+++
@@ -1,6 +0,0 @@
-function openDetail(id){
-document.querySelectorAll('.detail')
-.forEach(d=>d.style.display='none');
-document.getElementById(id).style.display='block';
-document.getElementById(id).scrollIntoView({behavior:'smooth'});
-}
